331bced82b0fa517aa7ff109b32a9210e95562ff
MGameDeveloper <82845930+MGameDeveloper@users.noreply.github.com>
2025-04-07 00:12:45 +0300

    Add files via upload

--- masPoolV2.h
+++ masPoolV2.h
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #define masAddrFrom(type, ptr, offset) (type*)(((uint8_t*)ptr) + offset)
 #define MAS_POOL_TAG ".masPool"
@@ -143,36 +144,47 @@ void* masPool_CopyItem(void* Item)
 /***************************************************************************************************************************
 *
 ****************************************************************************************************************************/
+struct masInputKey
+{
+    masInputKey *Next;
+    masInputKey *Prev;
+    int32_t      Key;
+};
 
 struct masInputAction
 {
-    char      Name[32];
-    void     *Func;
-    uint32_t *Key;
-    uint32_t  KeyCount;
-    uint32_t  KeyMod;
-    uint32_t  KeyState;
-    bool      bAllKeyMustBeActive;
+    masInputAction* Next;
+    masInputAction* Prev;
+
+    char         Name[32];
+    void        *Func;
+    masInputKey *KeyList;
+    uint32_t     KeyMod;
+    uint32_t     KeyState;
+    bool         bAllKeyMustBeActive;
 };
 
 struct masInputAxis
 {
-    char      Name[32];
-    void     *Func;
-    uint32_t *Key;
-    uint32_t  KeyCount;
-    float     AxisValue;
-    uint32_t  KeyMod;
-    bool      bAllKeyMustBeActive;
+    masInputAxis *Next;
+    masInputAxis *Prev;
+
+    char         Name[32];
+    void        *Func;
+    masInputKey *KeyList;
+    float        AxisValue;
+    uint32_t     KeyMod;
+    bool         bAllKeyMustBeActive;
 };
 
 struct masInputComp
 {
+    masInputComp   *Next;
+    masInputComp   *Prev;
+
     char            Name[32];
     masInputAction *Actions;
     masInputAxis   *Axes;
-    uint32_t        ActionCount;
-    uint32_t        AxisCount;
     bool            bConsumeEvent;
     bool            bBlockInput;
 };
@@ -186,10 +198,10 @@ struct masInputCompStack
 /***************************************************************************************************************************
 *
 ****************************************************************************************************************************/
-
-masPool *InputActionPool = masPool_Create("masInputAction", 32, sizeof(masInputAction));
-masPool *InputAxisPool   = masPool_Create("masInputAxis",   32, sizeof(masInputAxis));
-masPool *InputCompPool   = masPool_Create("masInputComp",   32, sizeof(masInputComp));
+masPool *InputKeyPool    = masPool_Create("masInputKey",    5000, sizeof(masInputKey));
+masPool *InputActionPool = masPool_Create("masInputAction", 32,   sizeof(masInputAction));
+masPool *InputAxisPool   = masPool_Create("masInputAxis",   32,   sizeof(masInputAxis));
+masPool *InputCompPool   = masPool_Create("masInputComp",   32,   sizeof(masInputComp));
 masInputCompStack InputCompStack[4] = { 0 }; // 4 Users Max
 
 masInputComp* masInputComp_Create(const char* Name, bool bConsumeEvent, bool bBlockInput)
@@ -203,10 +215,182 @@ masInputComp* masInputComp_Create(const char* Name, bool bConsumeEvent, bool bBl
 }
 void masInputComp_Destroy(masInputComp** InputComp)
 {
+    // todo: need revision
+    // validate InputComp
+
+    masInputComp* Comp = *InputComp;
+    
+    for ( ; Comp->Actions ; )
+    {
+        masInputAction* Temp = Comp->Actions;
+        Comp->Actions = Temp->Next;
+        for (; Temp->KeyList; )
+        {
+            masInputKey *TempKey = Temp->KeyList;
+            Temp->KeyList        = TempKey->Next;
+            masPool_FreeItem((void**)&TempKey);
+        }
+        masPool_FreeItem((void**)&Temp);
+    }
+
+    for ( ; Comp->Axes ; )
+    {
+        masInputAxis *Temp = Comp->Axes;
+        Comp->Axes         = Temp->Next;
+        for (; Temp->KeyList; )
+        {
+            masInputKey   *TempKey = Temp->KeyList;
+            Temp->KeyList          = TempKey->Next;
+            masPool_FreeItem((void**)&TempKey);
+        }
+        masPool_FreeItem((void**)&Temp);
+    }
+
+
     masPool_FreeItem((void**)&(*InputComp));
 }
 
-void masInputComp_AddAction(masInputComp* Comp, const char* Name, uint32_t KeyMod, uint32_t KeyState, const char* KeyListFmt, ...)
+
+#define MAS_KEY_LIST(...) #__VA_ARGS__##"\n", __VA_ARGS__
+
+void masInputComp_AddAction(masInputComp* Comp, const char* Name, void* ActionFunc, uint32_t KeyMod, uint32_t KeyState, bool bAllMustBeActive, const char* KeyListFmt, ...)
 {
+    uint32_t KeyListFmtLen = (uint32_t)::strlen(KeyListFmt);
+    if (KeyListFmtLen == 0)
+        return;
+    if (KeyListFmtLen == 1 && KeyListFmt[0] == '\n')
+        return;
+    uint32_t KeyCount = 0;
+    for (uint32_t i = 0; i < KeyListFmtLen; ++i)
+        if (KeyListFmt[i] == ',' || KeyListFmt[i] == '\n')
+            KeyCount++;
     
+    masInputAction* Action = (masInputAction*)masPool_NewItem(&InputActionPool);
+    if (!Action)
+        return;
+    ::memcpy(Action->Name, Name, ::strlen(Name));
+    Action->Func                = ActionFunc;
+    Action->KeyMod              = KeyMod;
+    Action->KeyState            = KeyState;
+    Action->bAllKeyMustBeActive = bAllMustBeActive;  
+
+    va_list Args;
+    va_start(Args, KeyCount);
+    for (uint32_t KeyIdx = 0; KeyIdx < KeyCount; ++KeyIdx)
+    {
+        // validate keys value
+        masInputKey* Key = (masInputKey*)masPool_NewItem(&InputKeyPool);
+        if (!Key)
+        {
+            masPool_FreeItem((void**)&Action);
+            return;
+        }
+
+        if (!Action->KeyList)
+            Action->KeyList = Key;
+        else
+        {
+            Key->Next             = Action->KeyList;
+            Action->KeyList->Prev = Key;
+            Action->KeyList       = Key;
+        }
+    }
+    va_end(Args);
+
+    if (!Comp->Actions)
+        Comp->Actions = Action;
+    else
+    {
+        Action->Next        = Comp->Actions;
+        Comp->Actions->Prev = Action;
+        Comp->Actions       = Action;
+    }
+}
+
+void masInputComp_AddAxis(masInputComp* Comp, const char* Name, void* AxisFunc, float AxisValue, uint32_t KeyMod, bool bAllMustBeActive, const char* KeyListFmt, ...)
+{
+    uint32_t KeyListFmtLen = (uint32_t)::strlen(KeyListFmt);
+    if (KeyListFmtLen == 0)
+        return;
+    if (KeyListFmtLen == 1 && KeyListFmt[0] == '\n')
+        return;
+    uint32_t KeyCount = 0;
+    for (uint32_t i = 0; i < KeyListFmtLen; ++i)
+        if (KeyListFmt[i] == ',' || KeyListFmt[i] == '\n')
+            KeyCount++;
+
+    masInputAxis* Axis = (masInputAxis*)masPool_NewItem(&InputAxisPool);
+    if (!Axis)
+        return;
+    ::memcpy(Axis->Name, Name, ::strlen(Name));
+    Axis->Func                = AxisFunc;
+    Axis->KeyMod              = KeyMod;
+    Axis->AxisValue           = AxisValue;
+    Axis->bAllKeyMustBeActive = bAllMustBeActive;
+
+    va_list Args;
+    va_start(Args, KeyCount);
+    for (uint32_t KeyIdx = 0; KeyIdx < KeyCount; ++KeyIdx)
+    {
+        // validate keys value
+        masInputKey* Key = (masInputKey*)masPool_NewItem(&InputKeyPool);
+        if (!Key)
+        {
+            masPool_FreeItem((void**)&Axis);
+            return;
+        }
+
+        if (!Axis->KeyList)
+            Axis->KeyList = Key;
+        else
+        {
+            Key->Next = Axis->KeyList;
+            Axis->KeyList->Prev = Key;
+            Axis->KeyList = Key;
+        }
+    }
+    va_end(Args);
+
+    if (!Comp->Axes)
+        Comp->Axes = Axis;
+    else
+    {
+        Axis->Next       = Comp->Axes;
+        Comp->Axes->Prev = Axis;
+        Comp->Axes       = Axis;
+    }
+}
+
+void masInputCompStack_Push(uint8_t EInputUser, masInputComp* InputComp)
+{
+    // validate EInputUser & InputComp
+
+    masInputCompStack* UserStack = &InputCompStack[EInputUser];
+    
+    if (!UserStack->Comps)
+        UserStack->Comps = InputComp;
+    else
+    {
+        InputComp->Next        = UserStack->Comps;
+        UserStack->Comps->Prev = InputComp;
+        UserStack->Comps       = InputComp;
+    }
+}
+
+void masInputCompStack_Pop(uint8_t EInputUser)
+{
+    // validate EInptUser
+
+    masInputCompStack* UserStack = &InputCompStack[EInputUser];
+
+    masInputComp* Comp = UserStack->Comps;
+    if (!Comp)
+        return;
+
+    if (Comp->Next)
+        UserStack->Comps = Comp->Next;
+    else
+        UserStack->Comps = NULL;
+
+    masInputComp_Destroy(&Comp);
 }
